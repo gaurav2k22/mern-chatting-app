6b06a927c9f08b2d4af2f9fe91132b724ef7d8cf
Gaurav Singh <am.singh.gaurav@gmail.com>
2024-09-09 15:07:51 -0700

    deployment

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,13 @@ import messageRoutes from './routes/message.route.js';
 import userRoutes from './routes/user.route.js';
 import connectToMongoDb from './db/connectToMongoDb.js';
 import { app, server } from './socket/socket.js';
+import path from 'path';
 
 
 
 const PORT= process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,6 +24,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend', 'dist', 'index.html'));
+});
 
 server.listen(PORT, ()=>{
     connectToMongoDb();
