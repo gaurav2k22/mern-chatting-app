01279c8c1e38027754d91391881da981d1627bdb
Gaurav Singh <166390276+gaurav2k22@users.noreply.github.com>
2024-09-10 08:07:49 -0700

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket= io(("http://localhost:8000"),{
+            const socket= io(("https://ping-nw9x.onrender.com"),{
                 query: {
                     userId: authUser._id
                 }
